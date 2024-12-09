23adb74f523f15b3af948f479ac1c985fe93cc0c
kaywish <kaywishawaan@gmail.com>
2024-12-09 14:26:37 -0500

    fixed favicon, added email

--- Fixera/inc/sendemail.php
+++ Fixera/inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "Don" );
+define( "RECIPIENT_EMAIL", "Eaglerepairsinc@gmail.com" );
 
 
 // Read the form values
